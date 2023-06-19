f1833322b7e61caa6dc07e2ba2cd690844519aec
Akshay Thakur <akshaymarx08@gmail.com>
2023-06-19 12:20:38 +0530

    change

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -20,9 +20,8 @@ const GetInTouch = () => {
       <Paragraph
         text={
           "It is our commitment to ensure a professional and enjoyable \
-                new home buying experience for you. \
-                If you want to get a home to start living as a family in an \
-                area that you love click the button below."
+                 experience for you. \
+                 "
         }
         maxWidth={"sm"}
         mx={0}
